3b464ebacabd354b622c6186f3401c3a74df0242
Taufik Riskyanto <taufik.riskyanto@doku.com>
2024-02-21 23:14:30 +0700

    add coverage test node js

--- test/exception.test.js
+++ test/exception.test.js
@@ -5,3 +5,8 @@ test("Exception Test", () =>{
     expect(() => callMe("Taufik")).toThrow(MyException);
     expect(() => callMe("Taufik")).toThrow("Ups my Exeption happens");
 })
+
+
+test("Success Test", () =>{
+    expect(callMe("Faqih")).toBe("OK");
+})
