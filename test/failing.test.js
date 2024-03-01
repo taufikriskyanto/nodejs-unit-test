be1884f97a332f98faf3eae5450f89b04c765e97
Taufik Riskyanto <taufik.riskyanto@doku.com>
2024-03-01 23:44:01 +0700

    add unit test failing jest

--- test/failing.test.js
+++ test/failing.test.js
@@ -1,5 +1,13 @@
 import { sayHello } from "../src/sayHello.js";
 
+
+//SUCCESS
+test("sayHello", ()=>{
+    expect(sayHello("Faqih")).toBe("Hello Faqih");
+})
+
+
+//failed
 test.failing("sayHello Error", ()=>{
     sayHello(null);
 })
