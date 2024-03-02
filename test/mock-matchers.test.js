adc1109d53182a484cbb7aaff329b3759370824e
Taufik Riskyanto <taufik.riskyanto@doku.com>
2024-03-02 10:45:49 +0700

    add unit test matchers jest

---
+++ test/mock-matchers.test.js
@@ -0,0 +1,12 @@
+import { calculate } from "../src/sum.js";
+
+test("test Calculate", ()=>{
+    const callback =  jest.fn();
+
+    calculate([10,10,10], callback);
+    calculate([10,10,10,10,10], callback);
+
+    expect(callback).toHaveBeenCalledTimes(2);
+    expect(callback).toHaveBeenCalledWith(30);
+    expect(callback).toHaveBeenCalledWith(50);
+})
